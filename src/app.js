23e4e76021fcabadcec9b5b492042912c518514c
Ashok Kumar Mahto <ashokkumarmahto2000@gmail.com>
2025-02-22 14:33:05 +0530

    created routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,35 @@
-const express =require('express');
+const express = require('express');
 
-const app =express();
+const app = express();
 
-app.use('/hello',(req,res)=>{
-res.send("Hello from Node")
+// different way to write routes
+// "use?r" => usr
+// "use*r" => useeklfd;dldkddr
+// "use+r" => useeddr
+// "u(se)+r" => useseseseddr
+
+app.get('/user', (req, res) => {
+    console.log(req.query);
+    
+    res.send({
+        Name: "Ashok Kumar",
+        city: "benglore"
+    })
 })
 
-app.listen(6666,()=>{
-console.log("Listening on prt 6666");
+app.post('/user', (req, res) => {
+    res.send("Data saved in DB")
+})
+
+app.delete('/user', (req, res) => {
+    res.send("User Deleted from DB")
+})
+
+app.use('/', (req, res) => {
+    res.send("Hello from Node")
+})
+
+app.listen(7777, () => {
+    console.log("Listening on prt 3000");
 
 })
